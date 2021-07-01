5994b68faa6fe87e540a0304690281d7220f6f36
Soheel Chughtai <soheel_chughtai@uk.ibm.com>
2021-07-01 09:45:38 +0100

    restructure to allow for two apps

---
+++ app/startemitter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/app/node_modules/ibmmq/redist/lib64
+export MQ_INSTALLATION_PATH=/app/node_modules/ibmmq/redist
+#export LD_LIBRARY_PATH=/opt/mqm/lib64
+#export MQ_INSTALLATION_PATH=/opt/mqm/redist
+DEBUG=mqcodeengine* node events/mqevents.js
